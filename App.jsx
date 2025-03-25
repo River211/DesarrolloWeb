a81b960427123f9b1a042ad436f5eb42d5240105
FRANKLINDANIEL <franklinmj23@outlook.com>
2025-03-25 18:08:37 -0400

    Componentes

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Bienvenido a la tienda</h1>
+      <p>Aquí podrás ver clientes, pedidos y detalles de pedidos.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ProductList.jsx
@@ -0,0 +1,13 @@
+const ProductList = () => {
+    return (
+      <div className="p-4">
+        <h2 className="text-xl font-bold">Lista de Productos</h2>
+        <ul>
+          <li>Producto 1</li>
+          <li>Producto 2</li>
+        </ul>
+      </div>
+    );
+  };
+  export default ProductList;
+  
